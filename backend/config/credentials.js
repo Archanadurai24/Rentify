457d45a2b3bd2deaca1e5d48bb9045c27227fe8d
archana <archanadurai24@gmail.com>
2024-05-20 18:54:34 +0530

    Initial commit

---
+++ backend/config/credentials.js
@@ -0,0 +1,15 @@
+const { mongo } = require("mongoose");
+
+const mongoURI = "mongodb+srv://archanadurai24:archana@cluster0.k0fbkh5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const email = "archanadurai24@gmail.com";
+const password = "0123456789";
+
+module.exports = {
+    mongoURI,
+    email,
+    password
+};
+
+
+//"mongodb+srv://jayesh007va:Jayesh2003@rentify.byfgrlv.mongodb.net/rentify";
+//const mongoURI = "mongodb+srv://archanadurai24:archana@cluster0.k0fbkh5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
